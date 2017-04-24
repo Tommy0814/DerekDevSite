0a383fc5947378aff3290c5258679636e82b9e4b
dirkdir <derekjdyer@sbcglobal.net>
2017-04-24 14:03:45 -0700

    Update app.js
    Updated server function

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/file', function(req, res) {
 
 //creates listener for server requests
 
-var server = app.listen(process.env.PORT || 5000), function() {
-    var port = server.address().port;
-    console.log("Express is working on port " + port);
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
